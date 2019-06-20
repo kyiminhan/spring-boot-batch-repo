a4c34d650115be7ecf24e2d5b8927644224a9003
KYIMINHAN <kyiminhan@gicjp.biz>
2019-06-20 18:14:16 +0900

    commit : adding the Spring Batch Quartz Java Config project

---
+++ spring-batch-007/src/main/java/com/kyiminhan/mm/spring/SpringBatch007Application.java
@@ -0,0 +1,13 @@
+package com.kyiminhan.mm.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatch007Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatch007Application.class, args);
+	}
+
+}
